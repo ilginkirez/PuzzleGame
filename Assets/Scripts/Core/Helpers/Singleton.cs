bfa2df065111d3656a71d6fec228d59ee427516d
Ilgın Kirez <ilginkirezl@gmail.com>
2025-08-07 17:31:37 +0300

    feat: add reusable Singleton base class for manager systems

---
+++ Assets/Scripts/Core/Helpers/Singleton.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace PuzzleGame.Core.Helpers
+
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+
+                    if (_instance == null)
+                    {
+                        GameObject singletonObj = new GameObject(typeof(T).Name);
+                        _instance = singletonObj.AddComponent<T>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+            }
+            else if (_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
